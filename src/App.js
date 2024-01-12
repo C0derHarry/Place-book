9dd74552c48e8ad317d54dd8d26ff77baca85bce
Harsher Gill <gillharsher1@gmail.com>
2024-01-12 11:02:40 +0530

    Added components, pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from "./user/pages/users";
+import NewPlace from "./places/pages/places";
+
+const App = () => {
+  return <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Redirect to="/"/>
+    </Switch>
+  </Router>;
+};
 
 export default App;
---
+++ src/user/componets/usersItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./user/components/usersItem.css"
+
+const UserItem = props => {
+
+};
+
+export default UserItem;
