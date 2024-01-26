d891256f362de82464010ab6e13ccd16c05141b6
Harsher Gill <gillharsher1@gmail.com>
2024-01-26 22:12:21 +0530

    add input validation

--- src/places/pages/newPlace.js
+++ src/places/pages/newPlace.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/components/util/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Nam Myo Ho Renge Kyo" />
+      <Input
+        element="input"
+        type="text"
+        label="Nam Myo Ho Renge Kyo"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a name"
+      />
     </form>
   );
 };
